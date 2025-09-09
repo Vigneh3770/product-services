d61d515508a0b32bb593db24a0542d5ef670552e
Vigneh3770 <156068248+Vigneh3770@users.noreply.github.com>
2025-09-09 23:48:57 +0530

    Delete products-service directory

--- products-service/src/model/index.js
+++
@@ -1,5 +0,0 @@
-const {createClient} = require("@supabase/supabase-js");
-const supabase = createClient(process.env.PROJECT_URL,process.env.API_KEY);
-
-//exporting the module
-module.exports = supabase
